89f1478a4fc30517cb949b693dc2dd04cc242490
vesta-12 <sofiya.serbina@narxoz.kz>
2026-04-14 10:00:11 +0500

    adding readme

---
+++ src/main/java/org/example/assigmentauth/AssigmentAuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.assigmentauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssigmentAuthApplication.class, args);
+    }
+
+}
