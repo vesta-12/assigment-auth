48910d6416a82884fe35e65eb4d699c35d018dac
vesta-12 <sofiya.serbina@narxoz.kz>
2026-04-14 10:20:52 +0500

    add main dto

---
+++ src/main/java/org/example/assigmentauth/dto/LoginResponse.java
@@ -0,0 +1,30 @@
+package org.example.assigmentauth.dto;
+
+public class LoginResponse {
+    private String message;
+    private String token;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String message, String token) {
+        this.message = message;
+        this.token = token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
